a0044c0904916fdbce64120a2c9d6a7529ad9cf0
jinkimdata <jinkimdata@gmail.com>
2016-10-12 11:45:08 -0400

    minor js edits

--- scripts/results-w-winner.js
+++ scripts/results-w-winner.js
@@ -6,8 +6,6 @@ function raceResults(races) {
     $.ajaxSetup({ cache: false });
 
     $.getJSON('http://data.baltimoresun.com/news/elections2016/json/generalResults.json', function(data){ 
-        var raceID;
-        var location;
         var html = [];
         var html2 = [];
         var html3 = [];
@@ -16,55 +14,52 @@ function raceResults(races) {
         var precinctsPercent;
         /* loop through array */
         for(var i = 0; i < races.length; i++){
-            raceID = races[i][0];
-            location = races[i][1];
             html = [];
             html2 = [];
             html3 = [];
-            $.each(data, function(index, key){    
-                if (key.fipscode == null & key.raceid == raceID) {
+            $.each(data, addToHTML(index,key, races[i][0], races[i][1]));
+        }
+        function addToHTML(index, key, raceID, location){            
+            if (key.fipscode === null & key.raceid == raceID) {
 
-                    /* identify percentage, multiply x 100 and round */
-                    var percent = Math.round((key.votepct) * 100);
+                /* identify percentage, multiply x 100 and round */
+                var percent = Math.round((key.votepct) * 100);
 
-                    var updated = new Date(key.lastupdated);
-                    updated = updated.toString();
-                    updated = updated.replace("GMT-0400", " ");
+                var updated = new Date(key.lastupdated);
+                updated = updated.toString();
+                updated = updated.replace("GMT-0400", " ");
 
-                    var votecount = key.votecount;
+                var votecount = key.votecount;
 
-                    /* if winner, add check */
-                    if (key.winner == true) {
-                        won = "winner"
-                    } else {
-                       won = ""
-                    };
-
-                    html.push("<table class=\"candidate-row\"><tr><td class=\"candidate name ", won, "\"> ", key.first, " ", key.last, "</td><td class=\"party-col\">", key.party, "</td><td class=\"votes\">", votecount.toLocaleString('en'), "</td><td class=\"percent\"><div class=\"percent-bar-bg\"><div class=\"percent-bar\" style=\"width:", percent, "%;\"></div></div><div class=\"vote-percent\">", percent, "%</div></td></tr></table>");
-
-                    //pulls the first updated by and displays it up top. not the best solution for sure.
-                    html2.push("<span class=\"update\">Updated: " + updated + "</span>");                
-
-
-                    $(location).html(html.join(''));
-                    $(".updated").html(html2);
-                    $( ".update" ).last().css( "display", "inline" );
-                    if ( html3.length == 0){
-
-                        precinctsReporting = key.precinctsreporting;
-                        precinctsTotal = key.precinctstotal;
-                        precinctsPercent = key.precinctsreportingpct;    
-    
-                        html3.push(" <div class=\"pre\"> " + precinctsReporting + " precincts reporting out of " + precinctsTotal +  ".</div>");                
-                    }
-                    $(location).append(html3);
+                /* if winner, add check */
+                if (key.winner === true) {
+                    won = "winner";
+                } else {
+                   won = "";
                 }
 
-            });
-        };
+                html.push("<table class=\"candidate-row\"><tr><td class=\"candidate name ", won, "\"> ", key.first, " ", key.last, "</td><td class=\"party-col\">", key.party, "</td><td class=\"votes\">", votecount.toLocaleString('en'), "</td><td class=\"percent\"><div class=\"percent-bar-bg\"><div class=\"percent-bar\" style=\"width:", percent, "%;\"></div></div><div class=\"vote-percent\">", percent, "%</div></td></tr></table>");
 
+                //pulls the first updated by and displays it up top. not the best solution for sure.
+                html2.push("<span class=\"update\">Updated: " + updated + "</span>");                
+
+
+                $(location).html(html.join(''));
+                $(".updated").html(html2);
+                $( ".update" ).last().css( "display", "inline" );
+                if ( html3.length === 0){
+
+                    precinctsReporting = key.precinctsreporting;
+                    precinctsTotal = key.precinctstotal;
+                    precinctsPercent = key.precinctsreportingpct;    
+
+                    html3.push(" <div class=\"pre\"> " + precinctsReporting + " precincts reporting out of " + precinctsTotal +  ".</div>");                
+                }
+                $(location).append(html3);
+            }
+        }
     });
-};
+}
 
 
 
